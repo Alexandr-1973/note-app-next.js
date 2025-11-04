b7aff5950044ddb94fbb3613b16c894212f2375d
Alexandr-1973 <144156771+Alexandr-1973@users.noreply.github.com>
2025-11-04 23:30:54 +0200

    test railway host

--- app/api/api.ts
+++ app/api/api.ts
@@ -1,6 +1,11 @@
 import axios from "axios";
 
+// export const api = axios.create({
+//   baseURL: "http://127.0.0.1:8000/api/",
+//   withCredentials: true,
+// });
+
 export const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: "https://note-app-backend-python-production.up.railway.app/api/",
   withCredentials: true,
 });
